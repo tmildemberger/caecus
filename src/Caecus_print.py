e7cf9f33e29653713b4669f235acfafb21638a52
Thiago MM <thimil@alunos.utfpr.edu.br>
2018-12-13 09:08:25 -0200

    primeiro teste

---
+++ src/Caecus_print.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import uno
+from com.sun.star.awt.MessageBoxButtons import BUTTONS_OK, BUTTONS_OK_CANCEL, BUTTONS_YES_NO, BUTTONS_YES_NO_CANCEL, BUTTONS_RETRY_CANCEL, BUTTONS_ABORT_IGNORE_RETRY
+from com.sun.star.awt.MessageBoxButtons import DEFAULT_BUTTON_OK, DEFAULT_BUTTON_CANCEL, DEFAULT_BUTTON_RETRY, DEFAULT_BUTTON_YES, DEFAULT_BUTTON_NO, DEFAULT_BUTTON_IGNORE
+from com.sun.star.awt.MessageBoxType import MESSAGEBOX, INFOBOX, WARNINGBOX, ERRORBOX, QUERYBOX
+
+from Caecus_print_UI import Caecus_print_UI
+
+# ----------------- helpers for API_inspector tools -----------------
+
+# uncomment for MRI
+#def mri(ctx, target):
+#    mri = ctx.ServiceManager.createInstanceWithContext("mytools.Mri", ctx)
+#    mri.inspect(target)
+
+# uncomment for Xray
+#def xray(myObject):
+#    try:
+#        sm = uno.getComponentContext().ServiceManager
+#        mspf = sm.createInstanceWithContext("com.sun.star.script.provider.MasterScriptProviderFactory", uno.getComponentContext())
+#        scriptPro = mspf.createScriptProvider("")
+#        xScript = scriptPro.getScript("vnd.sun.star.script:XrayTool._Main.Xray?language=Basic&location=application")
+#        xScript.invoke((myObject,), (), ())
+#        return
+#    except:
+#        raise _rtex("\nBasic library Xray is not installed", uno.getComponentContext())
+# -------------------------------------------------------------------
+
+
+class Caecus_print(Caecus_print_UI):
+    '''
+    Class documentation...
+    '''
+    def __init__(self):
+        Caecus_print_UI.__init__(self)
+
+        # --------- my code ---------------------
+        self.DialogModel.Title = "Caecus_print"
+        # mri(self.LocalContext, self.DialogContainer)
+
+    def myFunction(self):
+        # TODO: not implemented
+        pass
+
+        # --------- helpers ---------------------
+
+    def messageBox(self, MsgText, MsgTitle, MsgType=MESSAGEBOX, MsgButtons=BUTTONS_OK):
+        sm = self.LocalContext.ServiceManager
+        si = sm.createInstanceWithContext("com.sun.star.awt.Toolkit", self.LocalContext)
+        mBox = si.createMessageBox(self.Toolkit, MsgType, MsgButtons, MsgTitle, MsgText)
+        mBox.execute()
+
+    # -----------------------------------------------------------
+    #               Execute dialog
+    # -----------------------------------------------------------
+
+    def showDialog(self):
+        self.DialogContainer.setVisible(True)
+        self.DialogContainer.createPeer(self.Toolkit, None)
+        self.DialogContainer.execute()
+
+    # -----------------------------------------------------------
+    #               Action events
+    # -----------------------------------------------------------
+
+
+    def BotaoImprimir_OnClick(self):
+        self.DialogModel.Title = "It's Alive! - BotaoImprimir"
+        self.messageBox("It's Alive! - BotaoImprimir", "Event: OnClick", INFOBOX)
+        # TODO: not implemented
+
+
+
+def Run_Caecus_print(*args):
+    app = Caecus_print()
+    app.showDialog()
+
+g_exportedScripts = Run_Caecus_print,
---
+++ src/pythonpath/Caecus_print_UI.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+# =============================================================================
+#
+# Dialog implementation generated from a XDL file.
+#
+# Created: Wed Dec 12 22:20:01 2018
+#      by: unodit 0.7.0
+#
+# WARNING! All changes made in this file will be overwritten
+#          if the file is generated again!
+#
+# =============================================================================
+
+import uno
+import unohelper
+from com.sun.star.awt import XActionListener
+from com.sun.star.task import XJobExecutor
+
+class Caecus_print_UI(unohelper.Base, XActionListener, XJobExecutor):
+    """
+    Class documentation...
+    """
+    def __init__(self):
+        self.LocalContext = uno.getComponentContext()
+        self.ServiceManager = self.LocalContext.ServiceManager
+        self.Toolkit = self.ServiceManager.createInstanceWithContext("com.sun.star.awt.ExtToolkit", self.LocalContext)
+
+        # -----------------------------------------------------------
+        #               Create dialog and insert controls
+        # -----------------------------------------------------------
+
+        # --------------create dialog container and set model and properties
+        self.DialogContainer = self.ServiceManager.createInstanceWithContext("com.sun.star.awt.UnoControlDialog", self.LocalContext)
+        self.DialogModel = self.ServiceManager.createInstance("com.sun.star.awt.UnoControlDialogModel")
+        self.DialogContainer.setModel(self.DialogModel)
+        self.DialogModel.Height = 122
+        self.DialogModel.PositionY = "29"
+        self.DialogModel.Closeable = True
+        self.DialogModel.Name = "Imprimir_com_CAECUS"
+        self.DialogModel.PositionX = "85"
+        self.DialogModel.Width = 166
+        self.DialogModel.Moveable = True
+
+
+        # --------- create an instance of ComboBox control, set properties ---
+        self.PortaSerial = self.DialogModel.createInstance("com.sun.star.awt.UnoControlComboBoxModel")
+
+        self.PortaSerial.Dropdown = True
+        self.PortaSerial.Height = 11
+        self.PortaSerial.PositionY = "100"
+        self.PortaSerial.Name = "PortaSerial"
+        self.PortaSerial.PositionX = "8"
+        self.PortaSerial.TabIndex = 0
+        self.PortaSerial.Width = 70
+
+        # inserts the control model into the dialog model
+        self.DialogModel.insertByName("PortaSerial", self.PortaSerial)
+
+        # --------- create an instance of FixedText control, set properties ---
+        self.Label1 = self.DialogModel.createInstance("com.sun.star.awt.UnoControlFixedTextModel")
+
+        self.Label1.Height = 38
+        self.Label1.PositionY = "60"
+        self.Label1.Name = "Label1"
+        self.Label1.PositionX = "8"
+        self.Label1.Label = "Selecione AAa porta em que está conectada a impressora:"
+        self.Label1.TabIndex = 3
+        self.Label1.Width = 78
+        self.Label1.MultiLine = True
+        self.Label1.Align = 1
+
+        # inserts the control model into the dialog model
+        self.DialogModel.insertByName("Label1", self.Label1)
+
+        # --------- create an instance of FixedText control, set properties ---
+        self.Titulo = self.DialogModel.createInstance("com.sun.star.awt.UnoControlFixedTextModel")
+
+        self.Titulo.Height = 50
+        self.Titulo.Name = "Titulo"
+        self.Titulo.PositionY = "5"
+        self.Titulo.PositionX = "5"
+        self.Titulo.Label = "Imprimir em Braille com a impressora CAECUS"
+        self.Titulo.Width = 156
+        self.Titulo.TabIndex = 1
+
+        # inserts the control model into the dialog model
+        self.DialogModel.insertByName("Titulo", self.Titulo)
+
+        # --------- create an instance of Button control, set properties ---
+        self.BotaoImprimir = self.DialogModel.createInstance("com.sun.star.awt.UnoControlButtonModel")
+
+        self.BotaoImprimir.Height = 32
+        self.BotaoImprimir.PositionY = "80"
+        self.BotaoImprimir.Name = "BotaoImprimir"
+        self.BotaoImprimir.PositionX = "93"
+        self.BotaoImprimir.Label = "Imprimir"
+        self.BotaoImprimir.TabIndex = 2
+        self.BotaoImprimir.Width = 66
+
+        # inserts the control model into the dialog model
+        self.DialogModel.insertByName("BotaoImprimir", self.BotaoImprimir)
+
+        # add the action listener
+        self.DialogContainer.getControl('BotaoImprimir').addActionListener(self)
+        self.DialogContainer.getControl('BotaoImprimir').setActionCommand('BotaoImprimir_OnClick')
+
+    # -----------------------------------------------------------
+    #               Action events
+    # -----------------------------------------------------------
+
+    def actionPerformed(self, oActionEvent):
+        
+        if oActionEvent.ActionCommand == 'BotaoImprimir_OnClick':
+            self.BotaoImprimir_OnClick()
+
+
+# ----------------- END GENERATED CODE ----------------------------------------
